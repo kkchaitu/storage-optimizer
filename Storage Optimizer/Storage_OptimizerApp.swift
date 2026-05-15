5f059d97cc25cb8993076df6f6dbf8e0d35f49d3
Krishna Chaitanya <kkc.kambhampati@gmail.com>
2026-05-15 12:27:48 -0700

    Initial Commit

---
+++ Storage Optimizer/Storage_OptimizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Storage_OptimizerApp.swift
+//  Storage Optimizer
+//
+//  Created by Krishna Chaitanya Kambhampati on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Storage_OptimizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
